e18707af2ab590b2876c755041bb6be519d14d26
Michael Ye <micxye@yahoo.com>
2018-04-24 14:31:53 -0700

    connected client to db

--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const db = require('../database/db.js');
+const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 3004;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/../client/dist')));

8b756cd2113452cb470cd64e55eb7c186d640897
Michael Ye <micxye@yahoo.com>
2018-04-24 18:41:23 -0700

    refactor tests, 71% coverage

--- test/update.react.test.js
+++ test/update.react.test.js
@@ -9,6 +9,13 @@ import UpdatePostView from '../client/src/components/UpdatePostView.jsx';
 import CommentsFeed from '../client/src/components/CommentsFeed.jsx';
 import CommentItem from '../client/src/components/CommentItem.jsx';
 
+const testData = [{
+    "title": "Garden Brand Games",
+    "body": "Itaque ut at ab accusantium suscipit. Magni tempore id et quisquam itaque beatae quibusdam deleniti qui. Aliquid dolor sint voluptatem dolore. Labore voluptas suscipit est tempora recusandae animi sint nam. Sunt eum quasi ipsum consequatur quis. Ut doloribus perspiciatis nihil aspernatur ratione.\n \rModi sed saepe et sed velit laboriosam. Vero modi inventore in ex fugiat eaque accusamus. Rerum unde molestias voluptatem quae mollitia perspiciatis quasi natus et. Culpa in tempora laboriosam neque tempora vel deserunt sint.\n \rQuaerat ut cupiditate asperiores nostrum vero. Ducimus nostrum rerum est et sed. Est ut est quas et hic iure perspiciatis non quod. Tempore molestias accusamus a. Et accusamus eius qui illo soluta culpa molestias. Cumque ipsa facere.",
+    "date": "2018-01-05T10:02:51.899Z",
+    "likes": 43808,
+    "comments": []
+}]
 
 describe('default page view', () => {
     it('should render App', () => {
@@ -20,8 +27,6 @@ describe('default page view', () => {
         const component = shallow(<App />);
         expect((component).find(Updates).length).toBe(1);
     });
-
-    
 });
 
 describe('changeView method on App.jsx', () => {
@@ -32,22 +37,16 @@ describe('changeView method on App.jsx', () => {
     });
 
     it('should render updatePostView if anything else is passed in other than updates & comments', () => {
-        const wrapper = renderer.create(<App />);
-        const inst = wrapper.getInstance();
-        expect(inst.changeView('2nd update')).toMatchSnapshot();
+        const wrapper = mount(<App />).instance();
+        wrapper.setState({
+            updates: testData})
+        expect(wrapper.changeView('Garden Brand Games')).toMatchSnapshot();
     });
 });
 
-// describe('updates list view', () => {
-//     it('should limit words to 32 on update list items', () => {
-//         const component = mount(<Updates />);
-//         expect((component).find(document.getElementsByClassName(update-body).split(' ').length)).toHaveLength(32);
-//     });
-// });
-
-// describe('project comments feed', () => {
-//     it('should render 2 comments', () => {
-//         const component = mount(<CommentsFeed comments={testData.comments} />);
-//         expect((component).find(CommentItem)).toHaveLength(1);
-//     });
-// });
+describe('update list item', () => {
+    it('should render 1 item', () => {
+        const wrapper = shallow(<UpdateListItem update={testData[0]}/>);
+        expect(wrapper).toHaveLength(1);
+    });
+});

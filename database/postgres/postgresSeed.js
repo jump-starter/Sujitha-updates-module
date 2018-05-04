9ad4fe2ee803d0cda26821bbe77a6eff7995a3a4
Sujitha Vikram <sujithas875@gmail.com>
2018-05-04 14:11:04 -0700

    changed schema

--- database/postgres/postgresSeed.js
+++ database/postgres/postgresSeed.js
@@ -1,59 +1,89 @@
 const fs = require('fs');
 const faker = require('faker');
-var count = 0;
-const createDataProject = function(start, end, file) {
-    for (let i = start; i <= end; i++) {
-        var insertArr = [];
-        count++;
-        const project = `${i}|${faker.date.past()}`;
-        insertArr.push(project);
-        if (count  === 0) {
-            console.log ("project batch-", count);
-        }
-        fs.appendFileSync(file, insertArr +'\n');
-    }
-}
-const createDataUpdate = function(start, end, file) {
-    for (let j = 0; j <= 1; j++) {
-        for (let i = start; i <= end; i++) {
-            var insertArr = [];
-            count++;
-            const project = `${faker.random.number()}|${faker.date.past()}|${faker.lorem.sentence()}|${faker.random.number()}|
-            ${i}`;
-            insertArr.push(project);
-            if (count  === 0) {
-                console.log ("project batch-", count);
-            }
-            fs.appendFileSync(file, insertArr +'\n');
-        }
-    }
-}
-createDataUpdate(0, 1, './data2.txt');
 
-const createDataUpdateComment = function(start, end, file) {
-    for (let i = start; i <= end; i++) {
-        var insertArr = [];
-        count++;
-        const project = `${faker.random.number()}|${faker.date.past()}|${faker.lorem.sentence()}|${faker.image.avatar()}|
-        |${faker.internet.userName()}|${count}`;
+const createDataProject = function(start, end, file) {
+    var insertArr = [];
+    for (let i = start; i < end; i++) {
+        const project = `${i}|${faker.date.past().toISOString().slice(0, 19).replace('T', ' ')}`;
         insertArr.push(project);
-        if (count  === 0) {
-            console.log ("project batch-", count);
+        if (i % 1000 === 0) {
+            fs.appendFileSync(file, insertArr.join('\n') +'\n');
+            insertArr = [];
         }
-        fs.appendFileSync(file, insertArr +'\n');
     }
 }
+//createDataProject(0, 10000001, './data1.txt');
+createDataProject(0, 1, './text1.txt');
+
+
+const createDataUpdate = function(start, end, file) {  
+    var insertArr = [];
+    let j = -1; 
+    for (let i = start; i < end; i++) {
+        if (i % 2 === 0) {
+            j = j + 1;
+        }
+        const project = `${i}|${faker.date.past().toISOString().slice(0, 19).replace('T', ' ')}|${faker.random.words()}|${faker.lorem.sentence()}|${faker.random.number()}|\
+${j}`;
+        insertArr.push(project);
+        if (i % 1000 === 0) {
+            fs.appendFileSync(file, insertArr.join('\n') +'\n');
+            insertArr = [];
+        }
+    }
+}
+//createDataUpdate(0, 20000001, './data2.txt');
+createDataUpdate(0, 2, './text2.txt');
+
 const createDataComment = function(start, end, file) {
-    for (let i = start; i <= end; i++) {
-        var insertArr = [];
-        count++;
-        const project = `${faker.random.number()}|${faker.date.past()}|${faker.lorem.sentence()}|${faker.image.avatar()}|
-        |${faker.internet.userName()}|${count}`;
+    var insertArr = [];
+    for (let i = start; i < end; i++) {
+        const project = `${i}|${faker.date.past().toISOString().slice(0, 19).replace('T', ' ')}|${faker.lorem.sentence()}|${faker.image.avatar()}|\
+${faker.internet.userName()}`;
         insertArr.push(project);
-        if (count  === 0) {
-            console.log ("project batch-", count);
+        if (i % 1000 === 0) {
+            fs.appendFileSync(file, insertArr.join('\n') +'\n');
+            insertArr = [];
         }
-        fs.appendFileSync(file, insertArr +'\n');
     }
 }
-createDataProject(0, 1, './data1.txt');
+//createDataComment(0, 60000001, './data3.txt');
+createDataComment(0, 6, './text3.txt');
+
+const createDataCommentProject = function(start, end, file) {
+     var insertArr = [];
+     var j = -1;
+    for (let i = start; i < end; i++) {
+            if (i % 2 === 0) {
+                j = j + 1;
+            }
+        const project = `${i}|${j}|${i}`;
+        insertArr.push(project);
+        if (i % 1000 === 0) {
+           fs.appendFileSync(file, insertArr.join('\n') +'\n');
+           insertArr = [];
+        }
+    }
+}
+//createDataCommentProject(0, 20000001, './data3.txt');
+createDataCommentProject(0, 2, './text4.txt');
+
+const createDataCommentUpdate = function(start, end, file) {
+    var insertArr = [];
+    var j = -1;
+    var k = 20000001;
+    for (let i = start; i < end; i++) {
+        if (i % 2 === 0) {
+            j = j + 1;
+        }
+        const project = `${i}|${j}|${k}`;
+        insertArr.push(project);
+        if (i % 1000 === 0) {
+           fs.appendFileSync(file, insertArr.join('\n') +'\n');
+           insertArr = [];
+        }
+        k++;
+    }
+}
+//createDataCommentUpdate(0, 40000001, './data5.txt');
+createDataCommentUpdate(0, 4, './text5.txt');

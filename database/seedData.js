3c784f3851495a19fe29050352cb52891dc32bce
Sujitha Vikram <sujithas875@gmail.com>
2018-05-06 13:37:21 -0700

    adding data seeding file

---
+++ database/seedData.js
@@ -0,0 +1,31 @@
+const {createData} = require('./seed.js');
+
+// createData(0, 1000000, './data1.js');
+// console.log('added data!');
+
+// createData(1000001, 2000000, './data2.js');
+// console.log('added data!');
+
+// createData(2000001, 3000000, './data3.js');
+// console.log('added data!');
+
+// createData(3000001, 4000000, './data4.js');
+// console.log('added data!');
+
+// createData(4000001, 5000000, './data5.js');
+// console.log('added data!');
+
+// createData(5000001, 6000000, './data6.js');
+// console.log('added data!');
+
+// createData(6000001, 7000000, './data7.js');
+// console.log('added data!');
+
+// createData(7000001, 8000000, './data8.js');
+// console.log('added data!');
+
+// createData(8000001, 9000000, './data9.js');
+// console.log('added data!');
+
+// createData(9000001, 10000000, './data10.js');
+// console.log('added data!');

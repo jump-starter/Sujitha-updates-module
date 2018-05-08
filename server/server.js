f5145df68627d81946fdf8bd78e954bac4d2afed
Sujitha Vikram <sujithas875@gmail.com>
2018-05-08 16:26:25 -0700

    stress testing with artillery and new relic

--- server/server.js
+++ server/server.js
@@ -4,15 +4,8 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const db = require('../database/db.js');
 const cors = require('cors');
-const bluebird = require('bluebird');
-
-const redis = require('redis');
-bluebird.promisifyAll(redis.RedisClient.prototype);
-bluebird.promisifyAll(redis.Multi.prototype);
 
 const app = express();
-//const client = redis.createClient(6379, '172.17.0.3');
-const client = redis.createClient();
 const port = 3004;
 
 app.use(cors());
@@ -21,27 +14,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/project/:projectid', express.static(path.join(__dirname, '/../client/dist')));
 
 app.get('/api/updates/:id', (req, res) => {
-  client.getAsync(req.params.id)
-    .then((project) => {
-      if (project !== null) {
-        res.status(200);
-        res.send(project);
-      } else {
-        db.loadProject(req.params.id, (err, projects) => {
-          if (err) {
-            console.log('ERROR LOADING PROJECT', err);
-          } else {
-            const proj = JSON.stringify(projects)
-            res.send(projects);
-            client.set(req.params.id, proj);
-          }
-        })
-      };
-    })
-    .catch((err) => {
-      res.status(400);
-      res.send(err);
-    })
+  db.loadProject(req.params.id, (err, projects) => {
+    if (err) {
+      console.log('ERROR LOADING PROJECT', err);
+    } else {
+      const proj = JSON.stringify(projects)
+      res.send(projects);
+    }
+  });
 });
 
 app.listen(port, () => {
